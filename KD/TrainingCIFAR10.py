53d5fbe141e6e57b3bf3ddee6f189eb8ef7fc6db
westwing84 <nishihara-tsubasa-gn@ynu.jp>
2020-09-24 18:09:49 +0900

    Update Files

--- KD/TrainingCIFAR10.py
+++ KD/TrainingCIFAR10.py
@@ -16,9 +16,9 @@ import KDModel
 NUM_CLASSES = 10        # 分類するクラス数
 EPOCHS_T = 50            # Teacherモデルの学習回数
 EPOCHS_S = 200           # Studentモデルの学習回数
-BATCH_SIZE = 128        # バッチサイズ
+BATCH_SIZE = 512        # バッチサイズ
 VERBOSE = 2             # 学習進捗の表示モード
-optimizer = Adam()      # 最適化アルゴリズム
+optimizer = Adam(learning_rate=0.0005)      # 最適化アルゴリズム
 T = 5                   # 温度付きソフトマックスの温度
 ALPHA = 0.5             # KD用のLossにおけるSoft Lossの割合
 
@@ -42,54 +42,37 @@ validation_split = 0.2
 idx_split = int(x.shape[0] * (1 - validation_split))
 x_train, x_val = np.split(x, [idx_split])
 y_train, y_val = np.split(y, [idx_split])
+input_shape = x_train.shape[1:]
 
-'''
-# Trainデータを主データと補助データに分割．ValidationとTestデータは主データのみを残す．
-x_train_main, x_train_aux = np.array_split(x_train, 2, axis=1)
-x_val_main, x_val_aux = np.array_split(x_val, 2, axis=1)
-x_test_main, x_test_aux = np.array_split(x_test, 2, axis=1)
-'''
-x_train_main = x_train
-x_val_main = x_val
-x_test_main = x_test
-x_train_aux = np.zeros(x_train_main.shape)
-x_val_aux = np.zeros(x_val_main.shape)
-x_test_aux = np.zeros(x_test_main.shape)
-input_shape_main = x_train_main.shape[1:]
-input_shape_aux = x_train_aux.shape[1:]
 
-'''
 # 入力データの表示
-x_train_tmp = np.concatenate([x_train_main, 0*x_train_aux], axis=1)
-x_test_tmp = np.concatenate([x_test_main, 0*x_test_aux], axis=1)
 n = 10
 plt.figure()
 for i in range(n):
     ax = plt.subplot(2, n, i+1)
-    plt.imshow(x_train_tmp[i].reshape(32, 32, 3))
+    plt.imshow(x_train[i].reshape(32, 32, 3))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
     ax = plt.subplot(2, n, n + i + 1)
-    plt.imshow(x_test_tmp[i].reshape(32, 32, 3))
+    plt.imshow(x_test[i].reshape(32, 32, 3))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 plt.show()
-'''
+
 
 # MNISTデータセットをtf.data.Datasetに変換
-ds_train = tf.data.Dataset.from_tensor_slices((x_train_main, x_train_aux, y_train)).shuffle(x_train.shape[0]).batch(BATCH_SIZE)
-ds_val = tf.data.Dataset.from_tensor_slices((x_val_main, x_val_aux, y_val)).shuffle(x_val.shape[0]).batch(BATCH_SIZE)
-ds_test = tf.data.Dataset.from_tensor_slices((x_test_main, x_test_aux, y_test)).shuffle(x_test.shape[0]).batch(BATCH_SIZE)
+ds_train = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(x_train.shape[0]).batch(BATCH_SIZE)
+ds_val = tf.data.Dataset.from_tensor_slices((x_val, y_val)).shuffle(x_val.shape[0]).batch(BATCH_SIZE)
+ds_test = tf.data.Dataset.from_tensor_slices((x_test, y_test)).shuffle(x_test.shape[0]).batch(BATCH_SIZE)
 ds = tf.data.Dataset.zip((ds_train, ds_val))
 
 # Teacherモデルの定義
-inputs_main = Input(shape=input_shape_main)
-inputs_aux = Input(shape=input_shape_aux)
+inputs = Input(shape=input_shape)
 teacher = KDModel.Teacher(NUM_CLASSES, T)
-teacher_model = teacher.createModel(inputs_main, inputs_aux)
+teacher_model = teacher.createModel(inputs)
 
 # Teacherモデルの学習
 training = KDModel.NormalTraining(teacher_model)
@@ -102,15 +85,15 @@ for epoch in range(1, EPOCHS_T + 1):
     epoch_accuracy_val = CategoricalAccuracy()
 
     # 各バッチごとに学習
-    for (x_train_main_, x_train_aux_, y_train_), (x_val_main_, x_val_aux_, y_val_) in ds:
-        loss_value, grads = training.grad([x_train_main_, x_train_aux_], y_train_)
+    for (x_train_, y_train_), (x_val_, y_val_) in ds:
+        loss_value, grads = training.grad(x_train_, y_train_)
         optimizer.apply_gradients(zip(grads, teacher_model.trainable_variables))
-        loss_value_test = training.loss([x_val_main_, x_val_aux_], y_val_)
+        loss_value_test = training.loss(x_val_, y_val_)
 
         epoch_loss_avg(loss_value)
-        epoch_accuracy(y_train_, teacher_model([x_train_main_, x_train_aux_]))
+        epoch_accuracy(y_train_, teacher_model(x_train_))
         epoch_loss_avg_val(loss_value_test)
-        epoch_accuracy_val(y_val_, teacher_model([x_val_main_, x_val_aux_]))
+        epoch_accuracy_val(y_val_, teacher_model(x_val_))
 
     # 学習進捗の表示
     print('Epoch {}/{}: Loss: {:.3f}, Accuracy: {:.3%}, Validation Loss: {:.3f}, Validation Accuracy: {:.3%}'.format(
@@ -120,8 +103,8 @@ for epoch in range(1, EPOCHS_T + 1):
 
 # Studentモデルの定義
 student = KDModel.Students(NUM_CLASSES, T)
-student_hard_model = student.createHardModel(inputs_main)
-student_soft_model = student.createSoftModel(inputs_main)
+student_hard_model = student.createHardModel(inputs)
+student_soft_model = student.createSoftModel(inputs)
 
 # Studentモデルの学習
 student_hard_model.summary()
@@ -136,15 +119,15 @@ for epoch in range(1, EPOCHS_S + 1):
     epoch_accuracy_val = CategoricalAccuracy()
 
     # 各バッチごとに学習
-    for (x_train_main_, x_train_aux_, y_train_), (x_val_main_, x_val_aux_, y_val_) in ds:
-        loss_value, grads = kd.grad(x_train_main_, x_train_aux_, y_train_)
+    for (x_train_, y_train_), (x_val_, y_val_) in ds:
+        loss_value, grads = kd.grad(x_train_, y_train_)
         optimizer.apply_gradients(zip(grads, student_hard_model.trainable_variables))
-        loss_value_test = kd.loss(x_val_main_, x_val_aux_, y_val_)
+        loss_value_test = kd.loss(x_val_, y_val_)
 
         epoch_loss_avg(loss_value)
-        epoch_accuracy(y_train_, student_hard_model(x_train_main_))
+        epoch_accuracy(y_train_, student_hard_model(x_train_))
         epoch_loss_avg_val(loss_value_test)
-        epoch_accuracy_val(y_val_, student_hard_model(x_val_main_))
+        epoch_accuracy_val(y_val_, student_hard_model(x_val_))
 
     # 学習進捗の表示
     print('Epoch {}/{}: Loss: {:.3f}, Accuracy: {:.3%}, Validation Loss: {:.3f}, Validation Accuracy: {:.3%}'.format(
@@ -161,19 +144,19 @@ score_train = [Mean(), CategoricalAccuracy(), Precision(), Recall()]
 score_val = [Mean(), CategoricalAccuracy(), Precision(), Recall()]
 score_test = [Mean(), CategoricalAccuracy(), Precision(), Recall()]
 ds = tf.data.Dataset.zip((ds_train, ds_val, ds_test))
-for (x_train_main_, x_train_aux_, y_train_), (x_val_main_, x_val_aux_, y_val_), (x_test_main_, x_test_aux_, y_test_) in ds:
-    score_train[0](kd.loss(x_train_main_, x_train_aux_, y_train_))
-    score_val[0](kd.loss(x_val_main_, x_val_aux_, y_val_))
-    score_test[0](kd.loss(x_test_main_, x_test_aux_, y_test_))
-    score_train[1](y_train_, student_hard_model(x_train_main_))
-    score_val[1](y_val_, student_hard_model(x_val_main_))
-    score_test[1](y_test_, student_hard_model(x_test_main_))
-    score_train[2](y_train_, student_hard_model(x_train_main_))
-    score_val[2](y_val_, student_hard_model(x_val_main_))
-    score_test[2](y_test_, student_hard_model(x_test_main_))
-    score_train[3](y_train_, student_hard_model(x_train_main_))
-    score_val[3](y_val_, student_hard_model(x_val_main_))
-    score_test[3](y_test_, student_hard_model(x_test_main_))
+for (x_train_, y_train_), (x_val_, y_val_), (x_test_, y_test_) in ds:
+    score_train[0](kd.loss(x_train_, y_train_))
+    score_val[0](kd.loss(x_val_, y_val_))
+    score_test[0](kd.loss(x_test_, y_test_))
+    score_train[1](y_train_, student_hard_model(x_train_))
+    score_val[1](y_val_, student_hard_model(x_val_))
+    score_test[1](y_test_, student_hard_model(x_test_))
+    score_train[2](y_train_, student_hard_model(x_train_))
+    score_val[2](y_val_, student_hard_model(x_val_))
+    score_test[2](y_test_, student_hard_model(x_test_))
+    score_train[3](y_train_, student_hard_model(x_train_))
+    score_val[3](y_val_, student_hard_model(x_val_))
+    score_test[3](y_test_, student_hard_model(x_test_))
 f1_train = f1_score(score_train[2].result(), score_train[3].result())
 f1_val = f1_score(score_val[2].result(), score_val[3].result())
 f1_test = f1_score(score_test[2].result(), score_test[3].result())
--- KDModel.py
+++ KDModel.py
@@ -14,7 +14,7 @@ class Teacher():
         self.conv1 = Conv2D(filters=16, kernel_size=(1, 1), name='conv1')
         self.conv2 = Conv2D(filters=32, kernel_size=(3, 3), name='conv2')
         self.conv3 = Conv2D(filters=64, kernel_size=(5, 5), name='conv3')
-        self.maxpool = MaxPooling2D(pool_size=(2, 2))
+        self.maxpool = MaxPooling2D(pool_size=(2, 2), name='maxpooling1')
 
         self.dense1 = Dense(units=512, name='dense1')
         self.dense2 = Dense(units=512, name='dense2')
@@ -40,8 +40,11 @@ class Teacher():
 
         self.model = None
 
-    def createModel(self, inputs_main, inputs_aux):
-        x = concatenate([inputs_main, inputs_aux], axis=1)
+    def createModel(self, inputs_main, inputs_aux=None):
+        if inputs_aux == None:
+            x = inputs_main
+        else:
+            x = concatenate([inputs_main, inputs_aux], axis=1)
         x = self.relu1(self.batch_norm1(self.conv1(x)))
         x = self.relu2(self.batch_norm2(self.conv2(x)))
         x = self.relu3(self.batch_norm3(self.maxpool(self.conv3(x))))
@@ -49,7 +52,10 @@ class Teacher():
         x = self.relu4(self.batch_norm4(self.dense1(x)))
         x = self.relu5(self.batch_norm5(self.dense2(x)))
         outputs = self.softmax(self.dense3(x))
-        model = Model([inputs_main, inputs_aux], outputs, name='TeacherModel')
+        if inputs_aux == None:
+            model = Model(inputs_main, outputs, name='TeacherModel')
+        else:
+            model = Model([inputs_main, inputs_aux], outputs, name='TeacherModel')
         self.model = model
 
         return model
@@ -120,7 +126,23 @@ class KnowledgeDistillation():
         self.alpha = alpha
 
     @tf.function
-    def loss(self, x_main, x_aux, y_true):
+    def loss(self, x, y_true):
+        loss_object = CategoricalCrossentropy()
+        yt_soft = self.teacher_model(x)
+        ys_soft = self.student_soft_model(x)
+        ys_hard = self.student_hard_model(x)
+        loss_value = (1 - self.alpha) * loss_object(y_true, ys_hard) + \
+                     self.alpha * (self.temperature ** 2) * loss_object(yt_soft, ys_soft)
+        return loss_value
+
+    @tf.function
+    def grad(self, x, targets):
+        with tf.GradientTape() as tape:
+            loss_value = self.loss(x, targets)
+        return loss_value, tape.gradient(loss_value, self.student_hard_model.trainable_variables)
+
+    @tf.function
+    def loss_mainaux(self, x_main, x_aux, y_true):
         loss_object = CategoricalCrossentropy()
         yt_soft = self.teacher_model([x_main, x_aux])
         ys_soft = self.student_soft_model(x_main)
@@ -132,9 +154,9 @@ class KnowledgeDistillation():
         return loss_value
 
     @tf.function
-    def grad(self, x_main, x_aux, targets):
+    def grad_mainaux(self, x_main, x_aux, targets):
         with tf.GradientTape() as tape:
-            loss_value = self.loss(x_main, x_aux, targets)
+            loss_value = self.loss_mainaux(x_main, x_aux, targets)
         return loss_value, tape.gradient(loss_value, self.student_hard_model.trainable_variables)
 
 
